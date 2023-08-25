a9a14524757ea089bb8ef4539e69750aaa6726ed
Yerke Nurgali <70740293+qelephant@users.noreply.github.com>
2023-08-25 11:02:52 +0600

    Update LessonsGetter.java

--- src/main/java/com/epam/mjc/collections/combined/LessonsGetter.java
+++ src/main/java/com/epam/mjc/collections/combined/LessonsGetter.java
@@ -6,5 +6,11 @@ import java.util.Set;
 
 public class LessonsGetter {
     public Set<String> getLessons(Map<String, List<String>> timetable) {
+        Set<String> subjects = new HashSet<>();
+
+        for (List<String> lessons : timetable.values()) {
+            subjects.addAll(lessons);
+        }
+        return allLessons;
     }
 }
