0623866405bc7ba019692a39d895b345a9257d6f
Yerke Nurgali <70740293+qelephant@users.noreply.github.com>
2023-08-25 11:24:51 +0600

    Update DeveloperProjectFinder.java

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -1,10 +1,21 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class DeveloperProjectFinder {
-    public List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
+   public Map<Integer, Set<String>> createMap(Map<String, Integer> sourceMap) {
+    Map<Integer, Set<String>> result = new HashMap<>();
+
+    for (Map.Entry<String, Integer> entry : sourceMap.entrySet()) {
+        String key = entry.getKey();
+        int length = key.length();
+
+        if (!result.containsKey(length)) {
+            result.put(length, new HashSet<>());
+        }
+        result.get(length).add(key);
     }
+
+    return result;
+}
 }
