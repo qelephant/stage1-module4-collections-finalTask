d1e5cba036113e21a705a6268da4c3a388aaf52b
Yerke Nurgali <70740293+qelephant@users.noreply.github.com>
2023-08-25 11:50:01 +0600

    Update DeveloperProjectFinder.java

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -3,18 +3,23 @@ package com.epam.mjc.collections.combined;
 import java.util.*;
 
 public class DeveloperProjectFinder {
-   public Map<Integer, Set<String>> createMap(Map<String, Integer> sourceMap) {
-    Map<Integer, Set<String>> result = new HashMap<>();
-    for (Map.Entry<String, Integer> entry : sourceMap.entrySet()) {
-        String key = entry.getKey();
-        int length = key.length();
+   public List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
+        List<String> result = new ArrayList<>();
 
-        if (!result.containsKey(length)) {
-            result.put(length, new HashSet<>());
+        for (Map.Entry<String, Set<String>> entry : projects.entrySet()) {
+            String project = entry.getKey();
+            Set<String> developers = entry.getValue();
+            
+            if (developers.contains(developer)) {
+                result.add(project);
+            }
         }
-        result.get(length).add(key);
-    }
 
-    return result;
-   }
+        result.sort((p1, p2) -> {
+            int lengthComparison = Integer.compare(p2.length(), p1.length()); 
+            return lengthComparison != 0 ? lengthComparison : p2.compareTo(p1); 
+        });
+
+        return result;
+    }
 }
